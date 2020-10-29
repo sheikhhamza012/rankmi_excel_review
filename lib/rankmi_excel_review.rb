089b88bdbaace06ab66622d8bf90d5d765e7f2bd
sheikh hamza <sheikhhamza012@gmail.com>
2020-10-29 22:12:08 +0500

    initial commit

---
+++ lib/rankmi_excel_review.rb
@@ -0,0 +1,12 @@
+require "rankmi_excel_review/version"
+
+module RankmiExcelReview
+  class Error < StandardError; end
+  # Your code goes here...
+  class Main
+    def self.greet
+      puts "aa"
+
+    end
+  end
+end
---
+++ spec/rankmi_excel_review_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe RankmiExcelReview do
+  it "has a version number" do
+    expect(RankmiExcelReview::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
